60ccba31986ba27becc54e555bba1dc774c2b5ce
vGknight <vGknight@users.noreply.github.com>
2017-11-05 14:47:28 -0500

    Update smtpCfg.js

--- config/smtpCfg.js
+++ config/smtpCfg.js
@@ -6,14 +6,14 @@ let smtpCfg = {
     host: "smtp.sendgrid.net",
     auth: {
         // api_user: "apikey",
-        api_key: "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"
+        api_key: ""
     }
 };
 
 // mail optiosn for contact form
 let contactMail = {
     from: '"Blog Site!" no-reply@sendgrid.net>', // sender address
-    to: 'gk@snipinternet.com', // list of receivers
+    to: '', // list of receivers
     subject: 'Blogger Information Request', // Subject line
     text: ''			// plain text body
     // html: '<b></b>' 		// html body
